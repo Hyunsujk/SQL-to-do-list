6b8bba16d7e7112b9e77fcfe191b3036e5782a90
Hyunsujk <hyunsujk@gmail.com>
2020-04-03 18:10:13 -0500

    pool.js is complete

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "list" (
+"id" SERIAL PRIMARY KEY,
+"item" varchar(200),
+"complete" varchar(1)
+);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const listRouter = require("./routes/list.router");
+const listRouter = require("./routes/list.router");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(express.static("server/public"));
 
-// app.use("/list", listRouter);
+app.use("/list", listRouter);
 
 app.listen(PORT, () => {
   console.log("Server running on PORT:", PORT);
