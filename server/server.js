c0847eb0e287452b28f53dc5cf72b1c506ba8471
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-03 18:01:54 -0500

    server.js and port is established. test good

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const listRouter = require("./routes/list.router");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("server/public"));
+
+// app.use("/list", listRouter);
+
+app.listen(PORT, () => {
+  console.log("Server running on PORT:", PORT);
+});
