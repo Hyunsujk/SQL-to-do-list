40a099c3c044f46fafb80422a9699e2306999b75
Hyunsujk <hyunsujk@gmail.com>
2020-04-03 19:35:59 -0500

    clickComplete and updateItem function is added to let user to click the complete button and update the status of the item in the database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,7 @@ function init() {
   getList();
   $(".js-btn-add").on("click", clickAdd);
   $(".js-container").on("click", ".js-btn-delete", clickDelete);
+  $(".js-container").on("click", ".js-btn-complete", clickComplete);
 }
 
 //
@@ -24,6 +25,18 @@ function clickDelete(event) {
   console.log(this);
   const itemId = event.target.dataset.id;
   deleteItem(itemId);
+}
+
+function clickComplete(event) {
+  completedItemId = event.target.dataset.id;
+  const $itemRowElement = $(this).parent().parent();
+  const currentItem = $itemRowElement.children(".js-item").text().trim();
+  const currentStatus = "Y";
+  const completedItem = {
+    item: currentItem,
+    complete: currentStatus,
+  };
+  updateItem(completedItemId, completedItem);
 }
 //
 // API INTERACTION
@@ -74,12 +87,27 @@ function deleteItem(id) {
     });
 }
 
+function updateItem(id, completedItem) {
+  $.ajax({
+    method: "PUT",
+    url: `/list/${id}`,
+    data: completedItem,
+  })
+    .then((response) => {
+      getList();
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      console.warn("There was an error updating completed item");
+    });
+}
+
 function render() {
   $(".js-container").empty();
   for (let item of itemList) {
     $(".js-container").append(`
     <tr>
-    <td>${item.item}</td>
+    <td class ="js-item">${item.item}</td>
     <td><button class ="js-btn-complete" data-id="${item.id}">Complete</button></td>
     <td><button class ="js-btn-delete" data-id="${item.id}">Delete</button></td>
     </tr>`);
