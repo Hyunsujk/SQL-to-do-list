fac73b5c7e969d3287913d8cd9e42aa2ec3e8d05
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-05 12:08:15 -0500

    added comments

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,6 +1,8 @@
+// import pool
 const pg = require("pg");
 const Pool = pg.Pool;
 
+// save database information under pool
 const pool = new Pool({
   database: "weekend-to-do-app",
   host: "localhost",
@@ -9,12 +11,15 @@ const pool = new Pool({
   idleTimeoutMillis: 30000,
 });
 
+// if the database is connected, console.log that it's connected
 pool.on("connect", () => {
   console.log("Connected to Postgresql pool");
 });
 
+// if the database couldn't connect, console.log the error
 pool.on("error", (error) => {
   console.log("Error with Postgresql pool:", error);
 });
 
+// exports the pool
 module.exports = pool;
